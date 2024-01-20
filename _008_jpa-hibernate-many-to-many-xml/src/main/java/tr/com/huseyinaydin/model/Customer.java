93c7e9aa37759b97f2b67c9f435225658a376ac8
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-01-20 13:55:01 +0300

    Hibernate ile XML bazlı many-to-many yani çoğa çok projesi yazıldı.

---
+++ _008_jpa-hibernate-many-to-many-xml/src/main/java/tr/com/huseyinaydin/model/Customer.java
@@ -0,0 +1,63 @@
+package tr.com.huseyinaydin.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Hibernate.
+* 
+*/
+
+public class Customer {
+
+    private int customerId;
+    private String title;
+    private String name;
+    private Set<Address> addresses = new HashSet<Address>(0);
+
+    public Customer() {
+    }
+
+    public Customer(String title, String name, Set<Address> addresses) {
+        this.title = title;
+        this.name = name;
+        this.addresses = addresses;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+}
---
+++ _008_jpa-hibernate-many-to-many-xml/src/main/java/tr/com/huseyinaydin/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package tr.com.huseyinaydin.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Hibernate.
+* 
+*/
+
+public class HibernateUtil {
+
+    private static ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception ex) {
+            System.err.println("SessionFactory(oturum fabrikası nesnesi) oluşturulurken hata oldu : " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+}
